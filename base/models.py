91b1279db471f399bf77df274f45a9b100c1f8a3
Mohamed Shehata <63265699+shehata1999@users.noreply.github.com>
2023-09-11 05:13:02 +0300

    progress 1:24:56

--- base/models.py
+++ base/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+    #host = models.CharField(max_length=50)
+    #topic =
+    name = models.CharField(max_length=50)
+    description = models.TextField(null=True)
+    #participants = models.IntegerField(null=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
--- base/views.py
+++ base/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
+
+# rooms = [{'id':1,'name':'room1','capacity':10},
+#          {'id':2,'name':'room2','capacity':20},
+#          {'id':3,'name':'room3','capacity':30}
+#          ]
 
 def home(request):
-    return render(request, 'home.html')
-def room(request):
-    return render(request, 'room.html')
+    rooms = Room.objects.all()
+    context = {'rooms':rooms}
+    return render(request, 'base/home.html', context)
+def room(request,pk):
+    room = Room.objects.get(id=pk)
+    context = {'room':room}
+    return render(request, 'base/room.html',context)
 # Create your views here.
