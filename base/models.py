cdc269b2a4f59ec617f8e5d93720a934f07f237c
Mohamed Shehata <63265699+shehata1999@users.noreply.github.com>
2023-09-11 05:39:23 +0300

    before crud

--- base/models.py
+++ base/models.py
@@ -1,9 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
 class Room(models.Model):
-    #host = models.CharField(max_length=50)
-    #topic =
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=50)
     description = models.TextField(null=True)
     #participants = models.IntegerField(null=True)
@@ -12,3 +19,14 @@ class Room(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+class Message(models.Model):
+    user =  models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body[0:50]
