e8df5a126e0e7269428ee4b20eadc4a6ac25899b
Mohamed Shehata <63265699+shehata1999@users.noreply.github.com>
2023-09-22 12:12:49 +0300

    did some styling 3:18:50

--- base/views.py
+++ base/views.py
@@ -3,11 +3,12 @@ from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 
 # rooms = [{'id':1,'name':'room1','capacity':10},
@@ -72,6 +73,13 @@ def home(request):
 def room(request,pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created')
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        return redirect('room', pk=room.id)
     context = {'room':room,'room_messages':room_messages}
     return render(request, 'base/room.html',context)
 @login_required(login_url='login')
