0040f56c38e084cd75673a1e3984e0dd74a2adf4
Mohamed Shehata <63265699+shehata1999@users.noreply.github.com>
2023-09-23 18:35:18 +0300

    added user message controls

--- base/views.py
+++ base/views.py
@@ -73,14 +73,16 @@ def home(request):
 def room(request,pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all() 
     if request.method == 'POST':
         message = Message.objects.create(
             user=request.user,
             room=room,
             body=request.POST.get('body')
         )
+        room.participants.add(request.user)
         return redirect('room', pk=room.id)
-    context = {'room':room,'room_messages':room_messages}
+    context = {'room':room,'room_messages':room_messages,'participants':participants}
     return render(request, 'base/room.html',context)
 @login_required(login_url='login')
 def createRoom(request):
@@ -119,3 +121,13 @@ def deleteRoom(request,pk):
     context = {'obj':room}
     return render(request, 'base/delete.html', context)
 
+@login_required(login_url='login')
+def deleteMessage(request,pk):
+    message = Message.objects.get(id=pk)
+    if request.user != message.user:
+        return HttpResponse('You have no permission for this action')
+    if request.method == 'POST':
+        message.delete()
+        return redirect('room', pk=message.room.id)
+    context = {'obj':message}
+    return render(request, 'base/delete.html', context)
